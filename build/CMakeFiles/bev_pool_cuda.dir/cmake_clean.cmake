a392b2e63fcb9f5bdb7cbe7a051cf6fc7f1a68bd
YYH-GPU <1159089325@qq.com>
2026-05-26 19:48:59 +0800

    删除 build 文件夹

---
+++ build/CMakeFiles/bev_pool_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bev_pool_cuda.dir/optimize/bev_pool_cuda_generated_bev_pool_cuda.cu.o"
+  "libbev_pool_cuda.a"
+  "libbev_pool_cuda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/bev_pool_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/fastbev_video.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fastbev_video.dir/backproject/backproject.cpp.o"
+  "CMakeFiles/fastbev_video.dir/backproject/backproject.cpp.o.d"
+  "CMakeFiles/fastbev_video.dir/infer/fastbev_infer.cpp.o"
+  "CMakeFiles/fastbev_video.dir/infer/fastbev_infer.cpp.o.d"
+  "CMakeFiles/fastbev_video.dir/loader/nuscenes_loader.cpp.o"
+  "CMakeFiles/fastbev_video.dir/loader/nuscenes_loader.cpp.o.d"
+  "CMakeFiles/fastbev_video.dir/main_video.cpp.o"
+  "CMakeFiles/fastbev_video.dir/main_video.cpp.o.d"
+  "CMakeFiles/fastbev_video.dir/optimize/bev_pool.cpp.o"
+  "CMakeFiles/fastbev_video.dir/optimize/bev_pool.cpp.o.d"
+  "CMakeFiles/fastbev_video.dir/postprocess/postprocess.cpp.o"
+  "CMakeFiles/fastbev_video.dir/postprocess/postprocess.cpp.o.d"
+  "CMakeFiles/fastbev_video.dir/visualize/visualize.cpp.o"
+  "CMakeFiles/fastbev_video.dir/visualize/visualize.cpp.o.d"
+  "fastbev_video"
+  "fastbev_video.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fastbev_video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
